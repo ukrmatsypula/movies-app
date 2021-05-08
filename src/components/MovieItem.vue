f744418f4265758e535f31cc3363fe5c87ac62a4
Roman Matsypula <ukrmatsypula@gmail.com>
2021-05-08 20:42:04 +0300

    created and rendered first top 12  films

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -1,19 +1,88 @@
 <template>
-  <div>Movie {{ movie.Title }}</div>
+  <div class="movie-item mb-3">
+    <div class="movie-item-poster" :style="posterBg"></div>
+    <div class="movie-info-wrap d-flex flex-column justify-content-between">
+      <div class="movie-item-info">
+        <h3 class="movie-title">{{ movie.Title }}</h3>
+        <span class="movie-year">{{ movie.Year }}</span>
+      </div>
+      <div class="movie-item-controls row no-gutters">
+        <div class="col">
+          <BButton size="md" block variant="outline-light">Edit</BButton>
+        </div>
+        <div class="col">
+          <BButton size="md" block variant="outline-light">Remove</BButton>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'MovieItem',
+  name: "MovieItem",
   props: {
     movie: {
       default: Object,
       required: true,
     },
   },
-}
+  computed: {
+    posterBg() {
+      return {
+        backgroundImage: `url(${this.movie.Poster})`,
+      };
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
+.movie-item {
+  position: relative;
+  cursor: pointer;
+  border-radius: 5px;
+  overflow: hidden;
+  height: 400px;
+  transition: all 0.2s ease;
+}
 
+.movie-item:hover {
+  box-shadow: 0 5px 30px rgba(0, 0, 0, 0.7);
+  transform: scale(1.02);
+}
+
+.movie-item:hover .movie-info-wrap {
+  opacity: 1;
+  background: rgba(0, 0, 0, 0.7);
+}
+
+.movie-item-poster {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  z-index: -1;
+}
+
+.movie-title {
+  font-size: 18px;
+  color: #fff;
+}
+
+.movie-year {
+  font-size: 14px;
+  color: #fff;
+}
+
+.movie-info-wrap {
+  padding: 10px;
+  height: 100%;
+  opacity: 0;
+  transition: all .2s ease;
+}
 </style>
