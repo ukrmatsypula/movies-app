9614ccd2b59c9c6e439373cdfc9b7079c7a090a3
Roman Matsypula <ukrmatsypula@gmail.com>
2021-05-11 17:39:31 +0300

    added query params with vue router

--- src/App.vue
+++ src/App.vue
@@ -35,13 +35,23 @@ export default {
       "moviesLength",
     ]),
   },
+  watch: {
+    "$route.query": {
+      handler: "onPageQueryChange",
+      immediate: true,
+      deep: true,
+    },
+  },
   methods: {
     ...mapActions("movies", ["changeCurrentPage"]),
+    onPageQueryChange({ page = 1 }) {
+      this.changeCurrentPage(Number(page));
+    },
     onChangePoster(poster) {
       this.posterBg = poster;
     },
     onPageChanged(page) {
-      this.changeCurrentPage(page);
+      this.$router.push({ query: { page } });
     },
   },
 };
